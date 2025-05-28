2b641fa8df542623364f2bdc099ca9c1db791312
Rahul Kumar <rahulkumar9199270356@gmail.com>
2025-05-28 19:10:32 +0530

    chpater02 complete

---
+++ chpater02/03_operators.py
@@ -0,0 +1,46 @@
+# Arithmetic Operators
+
+a= 34
+
+b=4
+c = a+b
+print(c)
+
+
+#Assignment Operators
+
+a= 4-2
+b=6
+b+=3  # Increment the value of b by 3 and then assign it to b
+print(a)
+
+# comparison Operators 
+
+a = 5<4
+print(a)
+b = 6>5
+print(b)
+
+c = 5>=5
+print(c)
+
+
+#logical Operators
+
+e = True or False
+
+print(e)
+
+# Truth table of or
+print("True or false is ", True or False)
+print("True or True is ", True or  True)
+print("False or True is ", False or True)
+print("False or False is", False or False)
+
+# Truth table of and
+print("True and false is ", True and False)
+print("True and True is ", True and True)
+print("False and True is ", False and True)
+print("False and False is", False and False)  
+
+print(not(False))                                   
---
+++ chpater02/04_type_casting.py
@@ -0,0 +1,6 @@
+a = "31.2"
+
+b = float(a) # a but type should be float
+t = type(a)
+
+print(t)
